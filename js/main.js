eef5cdd760e6927b68c346cf6e5e18e19b50ee44
Sam <samandarusmanov1@gmail.com>
2024-10-08 22:47:03 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log(document)
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('../service-worker.js').then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            }).catch(error => {
+                console.error('Service Worker registration failed:', error);
+            });
+        });
+    }
+
+})
